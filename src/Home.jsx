6da1b8273b022373fabb20a7acd11f2de8ba263d
SamEstO <sy.estrada.o@gmail.com>
2022-12-12 14:27:24 -0600

    styled components y props

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 let Example = styled.div`
-    background-color: red;
     height: 200px;
     transition: all 0.2s;
+    padding: 50px;
+    
 `;
+let Button = styled.button`
+    border: solid 1px red;
+    background-color: transparent;
+    outline: 0;
+    font-size: 1em;
+    box-shadow: 2px 2px 2px rgba(0,0,0,0.6);
+    border-radius: 2px;
+`;
+
+let PrimaryButton = styled(Button)`
+    background-color: red;
+    border: 0;
+`;
+
+let AppLink = styled(Link)`
+    text-decoration: underline;
+    font-weigth: bold;
+    text-transform: uppercase;
+`;
+
+let AppInput = styled.input`
+    border: solid 1px red;
+`
 
 let HomePage = (props) => {
     return (
         <Example>
-            <p>Hola mundo!</p>
+            <Button>Hola mundo</Button>
+            <PrimaryButton>Boton primario</PrimaryButton>
+            <AppLink to='/videos'>Ir a videos</AppLink>
         </Example>
     )
 }
